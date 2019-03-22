44d82ed402cd5e41c9fc71267fd78f5afc30d640
Yoan Dinkov <yoandinkov@gmail.com>
2019-03-22 18:57:26 +0300

    Improving single experiment script.

--- experiments.py
+++ experiments.py
@@ -1,6 +1,8 @@
 from evaluation import evaluate_nn
 from data_retrieval import get_data
 from os import environ
+from tensorflow import set_random_seed
+from numpy.random import seed
 
 environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -10,36 +12,42 @@ data, labels, dataset = get_data()
 
 # Experiment options
 
-clf_type = 'nn'
+seed(61619)
+set_random_seed(25383)
+clf_type = 'nn' # lr, nn
 
-aggregation_options = 'avg'
+aggregation_options = 'avg' # avg, max
 
 split_options = {
-    'type': 'video',
-    'mean': False,
-    'config': 'IS09_emotion',
+    'type': 'video', # video, episodes
+    'mean': True, # True, False 
+    'config': 'IS09_emotion', #IS09_emotion,
+    'speech_embeddings': {
+        'mean': False # True, False
+    }
 }
 
 nn_arch = {
     'layers': [
         ('dropout', 0.2),
-        ('dense', 32, 'tanh'),
+        ('dense', 128, 'relu'),
         ('dropout', 0.2),
-        ('dense', 16, 'tanh'),
-        ('dropout', 0.2),
+        ('dense', 64, 'tanh'),
+        ('dropout', 0.20),
         ('dense', 3, 'softmax'),
     ],
-    'optimizer': 'adam',
+    'optimizer': 'adagrad',
     'batch_size': 75,
-    'epochs': 50
+    'epochs': 35
 }
 
 transformation_options = {
-    'fulltext': True,
-    'numerical': True,
-    'nela': True,
-    'v_tags': True,
-    'open_smile': True
+    'fulltext': 1, # 0,1
+    'numerical': 1, # 0,1
+    'nela': 1, # 0,1
+    'v_tags': 1, # 0,1
+    'open_smile': 1, # 0,1
+    'speech_embeddings': 1 # 0,1
 }
 
 evaluate_nn(data,
@@ -51,4 +59,4 @@ evaluate_nn(data,
             split_options=split_options,
             nn_arch=nn_arch,
             debug=True,
-            verbose=1)
+            verbose=0)
