6fabb3524d0c71e139b2618a98cac8b35493405e
Dinkov <yoandinkov@gmail.com>
2019-04-05 14:40:56 +0200

    Add tests.

---
+++ tests/prerequisites.py
@@ -0,0 +1,23 @@
+import keras
+import sklearn
+import pickle
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+
+
+def test_libs():
+    assert keras, 'keras missing'
+    assert sklearn, 'sklearn missing'
+
+
+def test_dataset():
+    dataset_path = environ['dataset']
+
+    assert dataset_path, 'dataset path not found'
+
+    with open(dataset_path, 'rb') as f:
+        dataset = pickle.load(f)
+
+    assert dataset, 'loading dataset failed.'
